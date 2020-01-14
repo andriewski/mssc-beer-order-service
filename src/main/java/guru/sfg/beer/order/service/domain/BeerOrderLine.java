a3db41da58efea96720c935dfaa6a58d4d0eb71d
Mark <plambiroo@gmail.com>
2020-01-14 21:47:06 +0300

    fixing beerOrder not saving upc

--- src/main/java/guru/sfg/beer/order/service/domain/BeerOrderLine.java
+++ src/main/java/guru/sfg/beer/order/service/domain/BeerOrderLine.java
@@ -37,10 +37,11 @@ public class BeerOrderLine extends BaseEntity {
 
     @Builder
     public BeerOrderLine(UUID id, Long version, Timestamp createdDate, Timestamp lastModifiedDate,
-                         BeerOrder beerOrder, UUID beerId, Integer orderQuantity, Integer quantityAllocated) {
+                         BeerOrder beerOrder, UUID beerId, String upc, Integer orderQuantity, Integer quantityAllocated) {
         super(id, version, createdDate, lastModifiedDate);
         this.beerOrder = beerOrder;
         this.beerId = beerId;
+        this.upc = upc;
         this.orderQuantity = orderQuantity;
         this.quantityAllocated = quantityAllocated;
     }
@@ -49,6 +50,7 @@ public class BeerOrderLine extends BaseEntity {
     private BeerOrder beerOrder;
 
     private UUID beerId;
+    private String upc;
     private Integer orderQuantity = 0;
     private Integer quantityAllocated = 0;
 }
