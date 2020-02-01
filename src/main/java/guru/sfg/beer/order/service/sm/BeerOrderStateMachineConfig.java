3a5b40809c1f63719c25b1ab95e6a694cc3ed8ff
Mark <plambiroo@gmail.com>
2020-02-01 17:48:03 +0300

    setting up allocation state machine States

---
+++ src/main/java/guru/sfg/beer/order/service/sm/BeerOrderStateMachineConfig.java
@@ -0,0 +1,26 @@
+package guru.sfg.beer.order.service.sm;
+
+import guru.sfg.beer.order.service.domain.BeerOrderEvent;
+import guru.sfg.beer.order.service.domain.BeerOrderStatus;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.statemachine.config.EnableStateMachineFactory;
+import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
+import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+
+import java.util.EnumSet;
+
+@Configuration
+@EnableStateMachineFactory
+public class BeerOrderStateMachineConfig extends StateMachineConfigurerAdapter<BeerOrderStatus, BeerOrderEvent> {
+
+    @Override
+    public void configure(StateMachineStateConfigurer<BeerOrderStatus, BeerOrderEvent> states) throws Exception {
+        states.withStates()
+                .initial(BeerOrderStatus.NEW)
+                .states(EnumSet.allOf(BeerOrderStatus.class))
+                .end(BeerOrderStatus.DELIVERED)
+                .end(BeerOrderStatus.DELIVERY_EXCEPTION)
+                .end(BeerOrderStatus.VALIDATION_EXCEPTION)
+                .end(BeerOrderStatus.ALLOCATION_EXCEPTION);
+    }
+}
